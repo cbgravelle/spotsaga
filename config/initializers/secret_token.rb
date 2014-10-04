3052109b5d4255fe0d0ec07a6f8c227db774991c
Corey Gravelle <cbgravelle@gmail.com>
2014-10-03 23:25:51 -0700

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spotsaga::Application.config.secret_key_base = 'ee5bc00cf9aa30f9799a2994614054e5b167f1948443f5825772d1aac8cb8f2292d8a5ee148482cd509412388c8bccf58a7dd96ab092e39717fb94b9947d53a8'
