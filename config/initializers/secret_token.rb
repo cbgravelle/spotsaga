d2d28479ab16b810b89fd9293b7523faa1dbe120
Corey Gravelle <cbgravelle@gmail.com>
2014-10-05 19:17:06 -0700

    Initialize Repo with MongoID, following http://moredevideas.com/getting-started-rails-4-with-mongodb/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spotsaga::Application.config.secret_key_base = '58ba003603bed4267c1e4ed7f9be7ed2d0b063f2826346198598cca3304b59e382c4ba065f06840439af254a5f4ec3c213cd7b82a83a7f6b56b41224bc5a9a4c'
